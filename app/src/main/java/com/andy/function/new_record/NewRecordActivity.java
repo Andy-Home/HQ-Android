b35fd8f5d03e504830d05aabd09bd3a4511baf23
Andy-Home <Andy_CLanguage@yeah.net>
2018-09-02 22:03:44 +0800

    添加账务记录（未完成）

---
+++ app/src/main/java/com/andy/function/new_record/NewRecordActivity.java
@@ -0,0 +1,20 @@
+package com.andy.function.new_record;
+
+import com.andy.BaseActivity;
+
+public class NewRecordActivity extends BaseActivity {
+    @Override
+    protected int getContentViewId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+}
